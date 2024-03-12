65096a705edbe91a94c19ca0b0b7c6889be07c43
WesleyVoss <wesleyvoss.net@gmail.com>
2024-03-12 16:03:00 -0300

    Alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 //  chamando a função do texto a ser imprimido.
 function exibiMensagemInicial() {
         exibirTextoNaTela('h1', 'Jogo do número secreto');
-        exibirTextoNaTela('p', 'Escolha um número entre 1 a 10.');
+        exibirTextoNaTela('p', 'Escolha um número entre 1 a 100.');
         responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
